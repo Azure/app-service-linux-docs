8c741ca5ed1e6a0ffa63d2c4466accf219cd23fe
Jeff Martinez <jeffxmartinez@gmail.com>
2023-04-04 15:51:49 -0700

    grpc demo files

---
+++ HowTo/gRPC/.NET/Demo/Server Stream/GrpcServerStreamClient/GrpcServerStreamClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServerStreamClient;
+
+
+// SERVER STREAM
+using var channel = GrpcChannel.ForAddress("https://grpcserverstream.azurewebsites.net/");
+
+var client = new Greeter.GreeterClient(channel);
+
+var cancelToken = new CancellationTokenSource(TimeSpan.FromMinutes(5));
+
+using (var call = client.SayHello(new HelloRequest { Name = " 1, 2, 3, 4, 5, 6, 7, 8, 9, 10" }))
+{
+    try
+    {
+        await foreach (var response in call.ResponseStream.ReadAllAsync(cancellationToken: cancelToken.Token))
+        {
+            Console.WriteLine(response.Message);
+        }
+    }
+    catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+    {
+        Console.WriteLine("Stream Cancelled");
+    }
+
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+};
---
+++ HowTo/gRPC/.NET/Demo/Unary/GrpcServiceClient/GrpcServiceClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServiceClient;
+
+
+using var channel = GrpcChannel.ForAddress("https://grpcserviceapp.azurewebsites.net/");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = ".NET Devs!" });
+
+Console.WriteLine(reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
